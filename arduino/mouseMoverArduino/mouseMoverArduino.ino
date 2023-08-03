b552ecaf5de469ac7372a4f81df18e93cf9e44be
Diego Bruno <lowendguru@gmail.com>
2023-08-04 00:20:01 +0200

    adding possibility to toggle on and off on restart

--- arduino/mouseMoverArduino/mouseMoverArduino.ino
+++ arduino/mouseMoverArduino/mouseMoverArduino.ino
@@ -1,8 +1,12 @@
 #include "Mouse.h"
+#include <EEPROM.h>
 
-int led = 13;         // the PWM pin the LED is attached to
+int led = 13;        // the PWM pin the LED is attached to
 int brightness = 0;  // how bright the LED is
 int fadeAmount = 5;  // how many points to fade the LED by
+
+int eepromAddress = 0;
+int toggle;
 
 unsigned long startMillis;
 unsigned long currentMillis;
@@ -17,29 +21,43 @@ void setup() {
 
   startMillis = millis();  //initial start time
   period = 240000;         // 4 minutes
+
+  toggle = EEPROM.read(eepromAddress);
+
+  if (toggle == 1) {
+    // ON
+    EEPROM.write(eepromAddress, 0);
+  } else {
+    // OFF
+    analogWrite(led, 0);
+    EEPROM.write(eepromAddress, 1);
+  }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  analogWrite(led, brightness);
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
+  if (toggle == 1) {
+    // set the brightness of pin 9:
+    analogWrite(led, brightness);
 
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 128) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(100);
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
 
-  currentMillis = millis();  //get the current time
-  if (currentMillis - startMillis >= period) {
-    // move mouse
-    Mouse.move(2, 0);
-    delay(50);
-    Mouse.move(-2, 0);
-    startMillis = currentMillis;
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness <= 0 || brightness >= 128) {
+      fadeAmount = -fadeAmount;
+    }
+    // wait for 30 milliseconds to see the dimming effect
+    delay(100);
+
+    currentMillis = millis();  //get the current time
+    if (currentMillis - startMillis >= period) {
+      // move mouse
+      Mouse.move(2, 0);
+      delay(50);
+      Mouse.move(-2, 0);
+      startMillis = currentMillis;
+    }
   }
 }
